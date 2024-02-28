aab7bc4be97e0fba12a0125299e0ec3893156574
muhkhr-tech <muhkhr.tech@gmail.com>
2024-02-29 07:38:38 +0800

    add button slider carousel

--- components/CarouselPaket.tsx
+++ components/CarouselPaket.tsx
@@ -1,28 +1,28 @@
 'use client'
-import Image from "next/image";
-import { useState } from "react";
-import { HiArrowLongLeft, HiArrowLongRight } from "react-icons/hi2";
+
+import { useRef } from "react";
+import { HiArrowLongLeft, HiArrowLongRight, HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 
 export default function CarouselPaket() {
-  const [scrollLeft, setScrollLeft] = useState(0)
+  const carouselRef = useRef<any>(null)
 
   const handleScrollLeft = () => {
-    setScrollLeft(scrollLeft + 1)
+    if (carouselRef.current) {
+       carouselRef.current.scrollLeft -= 200
+    }
+  }
+
+  const handleScrollRight = () => {
+    if (carouselRef.current) {
+      carouselRef.current.scrollLeft += 200
+   }
   }
 
   return (
     <div className="relative">
-      <div className="carousel carousel-center space-x-3 mt-4 border p-4 rounded-md">
+      <div ref={carouselRef} className="carousel carousel-center space-x-3 mt-4 border p-4 rounded-md">
         <div className="carousel-item">
           <img className="rounded-md w-52 sm:w-full" src="https://images.unsplash.com/photo-1575424909138-46b05e5919ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
-        </div>
-        <div className="carousel-item relative w-52 sm:w-72">
-          <Image
-            src={"/sara-dubler-Koei_7yYtIo-unsplash.jpg"}
-            layout="fill"
-            alt="Paket 4H3M"
-
-            className="rounded-md" />
         </div>
         <div className="carousel-item">
           <img className="rounded-md w-52 sm:w-full" src="https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
@@ -34,12 +34,14 @@ export default function CarouselPaket() {
           <img className="rounded-md w-52 sm:w-full" src="https://images.unsplash.com/photo-1590523277543-a94d2e4eb00b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80" />
         </div>
       </div>
-      {/* <button className="absolute top-1/2 left-2 -translate-y-1/2 z-10 btn btn-sm btn-circle">
-            <HiArrowLongLeft/>
-          </button>
-          <button className="absolute top-1/2 right-2 -translate-y-1/2 btn btn-sm btn-circle">
-            <HiArrowLongRight />
-          </button> */}
+      <button onClick={handleScrollLeft}
+        className="hidden sm:flex absolute top-1/2 left-2 -translate-y-1/2 z-10 btn btn-sm btn-circle">
+        <HiChevronLeft />
+      </button>
+      <button onClick={handleScrollRight}
+      className="hidden sm:flex absolute top-1/2 right-2 -translate-y-1/2 btn btn-sm btn-circle">
+        <HiChevronRight />
+      </button>
     </div>
   )
 }
